3e32c809030328c2e0b9ccbe3b45cab678353f51
Lutz Behnke <lutz.behnke@gmx.de>
2006-05-16 18:11:36 +0000

    lockdown

--- src/de/harper_hall/util/math/ByteHelper.java
+++ src/de/harper_hall/util/math/ByteHelper.java
@@ -5,6 +5,9 @@
  * 
  * $Id$
  * $Log$
+ * Revision 1.3  2006/05/16 18:11:36  behnke_l
+ * lockdown
+ *
  * Revision 1.2  2006/05/15 14:22:02  behnke_l
  * byteHelper tests and toByteArray function for ByteHelper
  *
@@ -27,6 +30,67 @@ public class ByteHelper {
     return (byte)((128-(val-128))*-1);
   }
   
+  /** convert a sequence of bytes to a integer value
+   * 
+   * @param arr
+   * @param offset
+   * @param lenght
+   * @param little_endian if true the values is taken to be represented as little endian, big endian if false
+   * @return integer value 
+   */
+  public static int intValue(byte[] arr, int offset, int length,boolean little_endian){
+    int ret =0,i,pos;
+    
+    if(length>4) throw new NumberFormatException("Max length for int value is 4 byte");
+    
+    if(little_endian){
+      pos  = offset;
+      for(i=0;i<length;i++,pos++){
+        ret*=256;
+        ret+=arr[pos];
+      }
+    }else{
+      pos  = offset+length-1;
+      for(i=0;i<length;i++,pos--){
+        ret*=256;
+        ret+=arr[pos];
+      }
+    }
+      
+    return ret;
+  }
+
+  /** convert a sequence of bytes to a integer value
+   * 
+   * @param arr
+   * @param offset
+   * @param lenght
+   * @param little_endian if true the values is taken to be represented as little endian, big endian if false
+   * @return integer value 
+   */
+  public static long longValue(byte[] arr, int offset, int length,boolean little_endian){
+    int i,pos;
+    long ret=0;
+    
+    if(length>4) throw new NumberFormatException("Max length for int value is 4 byte");
+    
+    if(little_endian){
+      pos  = offset;
+      for(i=0;i<length;i++,pos++){
+        ret*=256;
+        ret+=arr[pos];
+      }
+    }else{
+      pos  = offset+length-1;
+      for(i=0;i<length;i++,pos--){
+        ret*=256;
+        ret+=arr[pos];
+      }
+    }
+      
+    return ret;
+  }
+
   /**
    * same as calling toByteArray(i,true)
    * @param i integer value
@@ -58,7 +122,7 @@ public class ByteHelper {
    * @param i long value
    * @return a byte array containing the integer in little endian two-complement representation
    */
-  public byte[] toByteArray(long i){
+  public static byte[] toByteArray(long i){
     return toByteArray(i,true);
     }
   /**
@@ -66,7 +130,7 @@ public class ByteHelper {
    * @param little_endian if set to true the representation will be little endian, big endian otherwise.
    * @return a byte array containing the integer in little endian two-complement representation
    */
-  public byte[] toByteArray(long i,boolean little_endian){
+  public static byte[] toByteArray(long i,boolean little_endian){
     byte[] retval = new byte[8];
     if(little_endian){
       retval[7]= Long.valueOf(i%256).byteValue() ; i/=256;
@@ -94,11 +158,11 @@ public class ByteHelper {
    * @param i
    * @return a byte array containing the integer in little endian two-complement representation
    */
-  public byte[] toByteArray(byte i){return null;}
+  public static byte[] toByteArray(byte i){return null;}
   /**
    * @param i
    * @param little_endian if set to true the representation will be little endian, big endian otherwise.
    * @return a byte array containing the integer in little endian two-complement representation
    */
-  public byte[] toByteArray(byte i,boolean little_endian){return null;}
+  public static byte[] toByteArray(byte i,boolean little_endian){return null;}
 }
