61b64abb761e4974029e8c86a34cac4f26dd6ac8
Lutz Behnke <lutz.behnke@gmx.de>
2007-06-28 15:39:24 +0000

    Annotations hinzugefuegt. Ein wenig code fuer den Generator

---
+++ src/de/harper_hall/util/fea/CrudType.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package de.harper_hall.util.fea;
+
+/**
+ * @author sage
+ *
+ */
+public enum CrudType {
+	CREATE, READ, UPDATE, DELETE
+}
---
+++ src/de/harper_hall/util/fea/annotations/FeaDelete.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package de.harper_hall.util.fea.annotations;
+
+/**
+ * @author sage
+ *
+ */
+public @interface FeaDelete {
+	 String[] value();
+}
