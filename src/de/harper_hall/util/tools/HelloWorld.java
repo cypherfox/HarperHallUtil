c98cce4b0c1463175b1e7b61d0276393e00c03d0
Lutz Behnke <lutz.behnke@gmx.de>
2012-06-11 09:01:58 +0000

    add ClassFinder, clean up dead dir

--- src/de/harper_hall/util/tools/HelloWorld.java
+++ src/de/harper_hall/util/tools/HelloWorld.java
@@ -1,13 +1,8 @@
 /* This file is distributed under the GNU Public License 2.0 or a later version
  * at your discretion. See the file Copying for more information
  * 
- * copyright (c) 2005 Lutz Behnke <lutz.behnke@gmx.de> 
+ * copyright (c) 2005-2012 Lutz Behnke <lutz.behnke@gmx.de> 
  * 
- * $Id$
- * $Log$
- * Revision 1.1  2006/05/03 17:19:45  behnke_l
- * junit-target in build.xml
- *
  */
 package de.harper_hall.util.tools;
 
